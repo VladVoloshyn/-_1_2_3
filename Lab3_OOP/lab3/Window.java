8f8cc57490bcc0e8dcbedeec1d747d49367ccdf0
VladVoloshyn <90627712+VladVoloshyn@users.noreply.github.com>
2023-12-05 10:14:54 +0200

    Add files via upload

---
+++ Lab3_OOP/lab3/Window.java
@@ -0,0 +1,19 @@
+package lab3;
+
+import javax.swing.*;
+
+public class Window extends JFrame{
+    
+    public Window() {
+        setTitle("Snake Game");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(320, 345);
+        setLocation(400, 400);
+        add(new GameField());
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        Window w = new Window();
+    }
+}
